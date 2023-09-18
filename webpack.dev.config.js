a9961400bd31be36dc528f9f84828f8c8751fc2d
Aylenazucena <142960710+Aylenazucena@users.noreply.github.com>
2023-09-18 01:03:21 +0000

    ✨S02A01 Webpack 2

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -7,8 +7,6 @@ const path = require('path');
 // Exportamos un objeto de configuración
 // que sera usado por webpack
 module.exports = {
-  // 0. Estableciendo el modo produccion
-  mode: 'production',
   // 1. El archivo de entrada o indexador
   entry: "./client/index.js",
   // 2. Especificar el archivo de salida
@@ -16,7 +14,18 @@ module.exports = {
     // 2.1 Ruta absoluta de salida
     path: path.resolve(__dirname, "public"),
     // 2.2 Nombre del archivo de salida
-    filename: "bundle.js"
+    filename: "bundle.js",
+		// 2.3 Ruta base de archivos estaticos
+    publicPath: "/"
+  },
+  // 3. Configurando el servidor de desarrollo
+  devServer: {
+    // 3.1 Folder de archivos estáticos
+    static: path.join(__dirname, "public"),
+    // 3.2 Puerto del servidor de desarrollo
+    port: 8080,
+    // 3.3 Definiendo el host
+    host: "0.0.0.0"
   },
   // Agregando un modulo a webpack
   module: {
